4e4cbd34fca8c073989bcdc7b078690470201aa4
Lucas <lucaspadilhamata@hotmail.com>
2021-03-29 07:15:56 -0300

    Mudando a Variável de ambiente para development

--- app/Views/welcome_message.php
+++ app/Views/welcome_message.php
@@ -2,10 +2,10 @@
 <html lang="en">
 <head>
 	<meta charset="UTF-8">
-	<title>Welcome to CodeIgniter 4!</title>
+	<title>Projeto Moe</title>
 	<meta name="description" content="The small framework with powerful features">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link rel="shortcut icon" type="image/png" href="/favicon.ico"/>
+	<link rel="icon" href="https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/160/apple/271/open-book_1f4d6.png">
 
 	<!-- STYLES -->
 
